80417609521ebf23f9b4ca73929178ed00737d29
mTarmiziIsmail <57948525+Tarmizi08@users.noreply.github.com>
2023-07-25 16:32:20 +0800

    added a homepage

--- src/app/(SSR)/hello/page.tsx
+++ src/app/(SSR)/hello/page.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+
 const HelloPage = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return (
-    <div>
-      <h1>Hello Next 13.4 Image Gallery</h1>
+    <div className={"d-flex flex-column align-items-center"}>
+      <div>
+        <h1 className={"text-center"}>Hello from Next 13.4 Image Gallery</h1>
+      </div>
+      <Image
+        src={
+          "https://www.vectorkhazana.com/assets/images/products/SpongeBob_Faces.png"
+        }
+        alt={"image"}
+        width={500}
+        height={500}
+        className={"rounded shadow mw-100 h-100"}
+      />
     </div>
   );
 };
